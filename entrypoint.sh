c49b3206a5942d0f981a3882e1de76f69da507a0
hroyg <55093642+hroyg@users.noreply.github.com>
2020-07-19 15:06:59 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,8 +13,8 @@ WORKFLOW_JOBS_URL="https://api.github.com/repos/hroyg/slack-notify-workflow/acti
 workflow_success=true
 workflow_failure=false
 
-workflow_jobs=$(get_data $WORKFLOW_JOBS_URL) jq '.jobs[] |[select(.status == "completed") |{name,status,conclusion,id,run_id,started_at}]|sort_by(.started_at)')
-echo $workflow_jobs  jq .
+workflow_jobs=$(get_data $WORKFLOW_JOBS_URL | jq '.jobs[] |[select(.status == "completed") |{name,status,conclusion,id,run_id,started_at}]|sort_by(.started_at)')
+echo $workflow_jobs | jq .
 
 
 echo $workflow_jobs  |jq  -c -r '.[]| .conclusion'| \
