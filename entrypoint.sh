4346327446f2db031e850f2486812b4aac125112
hroyg <55093642+hroyg@users.noreply.github.com>
2020-07-19 15:10:56 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 
 get_data(){
 #debug
-   echo "curl -sL -H 'Cache-Control: no-cache' -H 'Accept: application/vnd.github.v3+json'  ${WORKFLOW_JOBS_URL}"
-         curl -sL -H 'Cache-Control: no-cache' -H 'Accept: application/vnd.github.v3+json'  ${WORKFLOW_JOBS_URL}     # check if authentication is needed 
+   echo "curl -sL -H 'Cache-Control: no-cache' -H 'Accept: application/vnd.github.v3+json'  $1"
+         curl -sL -H 'Cache-Control: no-cache' -H 'Accept: application/vnd.github.v3+json'  $1     # check if authentication is needed 
 }
 
 
@@ -13,7 +13,8 @@ WORKFLOW_JOBS_URL="https://api.github.com/repos/hroyg/slack-notify-workflow/acti
 workflow_success=true
 workflow_failure=false
 
-workflow_jobs=$(get_data $WORKFLOW_JOBS_URL)# | jq '.jobs[] |[select(.status == "completed") |{name,status,conclusion,id,run_id,started_at}]|sort_by(.started_at)')
+workflow_jobs=$(get_data ${WORKFLOW_JOBS_URL})
+# | jq '.jobs[] |[select(.status == "completed") |{name,status,conclusion,id,run_id,started_at}]|sort_by(.started_at)')
 echo $workflow_jobs | jq .
 
 
