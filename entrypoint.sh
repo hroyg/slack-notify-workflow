94a77160432e5f62db76c77b08ec8459cb6e4d26
hroyg <55093642+hroyg@users.noreply.github.com>
2020-07-20 20:00:53 +0300

    updating repository url , repo name to be dynamic

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,45 +7,45 @@ get_data(){
 }
 
 
-WORKFLOW_JOBS_URL="https://api.github.com/repos/hroyg/slack-notify-workflow/actions/runs/$3/jobs" 
+WORKFLOW_JOBS_URL="https://api.github.com/repos/$3/actions/runs/$2/jobs" 
 workflow_success=true
 workflow_failure=false
 
 workflow_jobs=$(get_data ${WORKFLOW_JOBS_URL} | jq '[.jobs[] | select(.status == "completed") | {name,conclusion,id,run_id,started_at}] |sort_by(.started_at)')
 #########DEBUG########
-echo $workflow_jobs |jq '.[]'
-echo $workflow_jobs |jq '.[] | .conclusion'
+#echo $workflow_jobs |jq '.[]'
+#echo $workflow_jobs |jq '.[] | .conclusion'
 #########DEBUG#########
 jobs_conclusion=$(echo $workflow_jobs |jq -r -c '.[] | .conclusion')
 for conclusion in $jobs_conclusion ; do
-      echo  $conclusion #DEBUG
+#      echo  $conclusion                                                                             #DEBUG
       if [[ $conclusion == "cancelled" ]] ; then
         workflow_success=false
-        echo  "------------------------CANCELLED------------------"
+        echo  "------------------------CANCELLED------------------"                                  #DEBUG
         break
       fi
 
       if [[ $conclusion == "failure"  ]] ; then
         workflow_failure=true
-        echo  "------------------------FAILURE------------------"
+        echo  "------------------------FAILURE------------------"                                     #DEBUG
         failed_job_step=$(echo $workflow_jobs |jq -r '.[]|select(.conclusion == "failure") | .steps[] | select(.conclusion == "failure") | .name')
         break
       fi
       
-      echo  "------------------------SUCCESS------------------"
+      echo  "------------------------SUCCESS------------------"                                        #DEBUG
 done
 
 
 case "${workflow_success},${workflow_failure}" in                                                        
+                                                                                                                                                                                                                 
+  false,false)   echo "::set-output name=workflow_result::cancelled"  ;;                           
                                                                                                          
+  true,true  )   echo "::set-output name=workflow_result::failure"                                
+                 echo "::set-output name=failed_step::$failed_job_step"                           
+                 echo $failed_job_step  ;;                                                          
                                                                                                          
-       false,false)    echo A#"::set-output name=workflow_result::cancelled"  ;;                           
-                                                                                                         
-       true,true)      echo B#"::set-output name=workflow_result::failure"                                
-                        echo C#"::set-output name=failed_step::$failed_job_step"                           
-                        echo $failed_job_step;;                                                          
-                                                                                                         
-                *)      echo E#"::set-output name=workflow_result::success"  ;;                            
+  *          )   echo "::set-output name=workflow_result::success"  ;;                            
+
 esac
 
 
