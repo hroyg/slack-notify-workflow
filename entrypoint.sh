66c634dab4d1507e89964c6bbe67f14e4ec9a151
hroyg <55093642+hroyg@users.noreply.github.com>
2020-07-20 19:30:13 +0300

    fixing the command substitution

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,40 +13,40 @@ workflow_failure=false
 
 workflow_jobs=$(get_data ${WORKFLOW_JOBS_URL} | jq '[.jobs[] | select(.status == "completed") | {name,conclusion,id,run_id,started_at}] |sort_by(.started_at)')
 #########DEBUG########
-#echo $($workflow_jobs |jq '.[]')
-#echo $($workflow_jobs |jq '.[] | .conclusion')
+echo $workflow_jobs |jq '.[]'
+echo $workflow_jobs |jq '.[] | .conclusion'
 #########DEBUG#########
-#jobs_conclusion=$(echo $workflow_jobs |jq -r -c '.[] | .conclusion')
-#for conclusion in $jobs_conclusion ; do
-  #    echo  $conclusion #DEBUG
-  #    if [[ $job_conclusion == "cancelled" ]] ; then
-  #      workflow_success=false
-  #      echo  "------------------------CANCELLED------------------"
-  #      break
-  #    fi
+jobs_conclusion=$(echo $workflow_jobs |jq -r -c '.[] | .conclusion')
+for conclusion in $jobs_conclusion ; do
+      echo  $conclusion #DEBUG
+      if [[ $job_conclusion == "cancelled" ]] ; then
+        workflow_success=false
+        echo  "------------------------CANCELLED------------------"
+        break
+      fi
 
- #     if [[ $job_conclusion == "failure"  ]] ; then
- #       workflow_failure=true
- #       echo  "------------------------FAILURE------------------"
- #       failed_job_step=$(echo $workflow_jobs |jq -r '.[]|select(.conclusion == "failure") \
- #       | .steps[] | select(.conclusion == "failure") | .name')
-  #      break
- #     fi
+      if [[ $job_conclusion == "failure"  ]] ; then
+        workflow_failure=true
+        echo  "------------------------FAILURE------------------"
+        failed_job_step=$(echo $workflow_jobs |jq -r '.[]|select(.conclusion == "failure") \
+        | .steps[] | select(.conclusion == "failure") | .name')
+        break
+      fi
       
-#      echo  "------------------------SUCCESS------------------"
-#done
+      echo  "------------------------SUCCESS------------------"
+done
 
 
-#case "${workflow_success},${workflow_failure}" in                                                        
+case "${workflow_success},${workflow_failure}" in                                                        
                                                                                                          
-#                                                                                                         
-#       false,false)    echo A#"::set-output name=workflow_result::cancelled"  ;;                           
-#                                                                                                         
-#       true,true)      echo B#"::set-output name=workflow_result::failure"                                
-#                        echo C#"::set-output name=failed_step::$failed_job_step"                           
-#                        echo $failed_job_step;;                                                          
-#                                                                                                         
-#                *)      echo E#"::set-output name=workflow_result::success"  ;;                            
-#esac
+                                                                                                         
+       false,false)    echo A#"::set-output name=workflow_result::cancelled"  ;;                           
+                                                                                                         
+       true,true)      echo B#"::set-output name=workflow_result::failure"                                
+                        echo C#"::set-output name=failed_step::$failed_job_step"                           
+                        echo $failed_job_step;;                                                          
+                                                                                                         
+                *)      echo E#"::set-output name=workflow_result::success"  ;;                            
+esac
 
 
